e68ba66e9097144de5db57ab182ff8a35ced40db
Aden Seaman <aden@seamanfamily.ca>
2018-03-19 14:48:37 -0400

    WIP: Experimenting with new key layout and UI

--- xmonad.hs
+++ xmonad.hs
@@ -6,7 +6,7 @@ import XMonad.Util.Run -- for spawnPipe and hPutStrLn
 import XMonad.Hooks.ManageDocks -- avoid xmobar
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
-import XMonad.Actions.WindowBringer
+import XMonad.Actions.GridSelect
 import XMonad.Actions.DynamicWorkspaces
 import XMonad.Actions.CopyWindow(copy)
 import XMonad.Actions.CycleWS
@@ -15,10 +15,13 @@ import System.Process
 import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe)
 import XMonad.Layout.Tabbed
-import XMonad.Layout.LayoutBuilder
 import XMonad.Layout.Renamed
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.SubLayouts
+import XMonad.Layout.BoringWindows
+import XMonad.Layout.WindowNavigation
+import Data.Map as M
 
 myNormalBorderColor = "#000000"
 myFocusedBorderColor = "#ff0000"
@@ -27,6 +30,8 @@ myStatusBar screenNum = "/home/aden/bin/xmobar -x" ++ (show screenNum) ++ " /hom
 myWorkspaces = ["mail", "browser"]
 
 modm = mod4Mask
+
+gsconfig1 = defaultGSConfig { gs_cellheight = 60, gs_cellwidth = 200, gs_font = "xft:Sans-6", gs_navigate = substringSearch defaultNavigation }
 
 main = do
   maxScreenIndex <- ((-) 1) <$> fromMaybe 1 <$> (readMaybe <$> readCreateProcess (shell "xrandr | grep connected | grep -v disconnected | wc -l") "")
@@ -40,33 +45,70 @@ main = do
     , borderWidth = 3
     , logHook = myLogHook xmprocList
     , workspaces = myWorkspaces
-    } `additionalKeys` myKeys
+    , XMonad.keys = myKeys
+    }
 
 -- to find default key bindings, look in: https://hackage.haskell.org/package/xmonad-0.13/docs/src/XMonad-Config.html
-myKeys = [
-    ((modm, xK_g                    ), gotoMenu)
+myKeys = \conf -> M.fromList $ [
+    ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf) -- Launch terminal
+  -- Window Focus
+  , ((modm, xK_u     ), focusUp >> warpToWindow') -- Move focus up in window list
+  , ((modm, xK_j     ), focusDown >> warpToWindow') -- Move focus down in window list
+  , ((modm, xK_m     ), windows W.focusMaster >> warpToWindow' ) -- Move focus to the master window
+  , ((modm .|. controlMask, xK_u), onGroup W.focusUp') -- shift tab group focus up its list
+  , ((modm .|. controlMask, xK_j), onGroup W.focusDown') -- shift tab group focus down its list
+  , ((modm, xK_question), goToSelected gsconfig1) -- find a window and focus on it
+  , ((modm .|. shiftMask, xK_slash ), goToSelected gsconfig1) -- find a window and focus on it
+  -- Window adjustment
+  , ((modm .|. shiftMask, xK_u     ), windows W.swapUp    ) -- move the window up the list
+  , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  ) -- move the window down the list
+  , ((modm .|. shiftMask, xK_b     ), kill) -- close the focused window
+  , ((modm .|. shiftMask, xK_h     ), sendMessage $ Toggle FULL) -- toggle mirroring of active layout
+  , ((modm .|. shiftMask, xK_m     ), windows W.swapMaster) -- Swap the focused window and the master window
+  -- Layout adjustment
+  , ((modm, xK_equal ), sendMessage (IncMasterN 1)) -- Increment the number of windows in the master area
+  , ((modm, xK_minus), sendMessage (IncMasterN (-1))) -- Deincrement the number of windows in the master area
+  , ((modm, xK_bracketright), sendMessage Expand) -- Expand the master area
+  , ((modm, xK_bracketleft), sendMessage Shrink) -- Shrink the master area
+  , ((modm, xK_backslash ), sendMessage $ Toggle MIRROR) -- toggle mirroring of active layout
+  -- Group adjustment
+  -- , ((modm .|. shiftMask, xK_o), withFocused (sendMessage . MergeAll)) -- merge all windows
+  -- , ((modm .|. shiftMask, xK_l), withFocused (sendMessage . MergeAll)) -- merge all windows
+  , ((modm .|. shiftMask .|. controlMask, xK_u), sendMessage $ pullGroup U)
+  , ((modm .|. shiftMask .|. controlMask, xK_j), sendMessage $ pullGroup D)
+  , ((modm .|. shiftMask .|. controlMask, xK_k), withFocused (sendMessage . UnMerge))
+  -- Workspace modification
   , ((modm .|. shiftMask, xK_BackSpace), removeWorkspace)
-  , ((modm .|. shiftMask, xK_t      ), renameWorkspace def)
-  , ((modm .|. shiftMask, xK_s      ), selectWorkspace def)
-  , ((modm .|. shiftMask, xK_m      ), withWorkspace def (windows . W.shift))
-  , ((modm, xK_minus), prevWS  ) -- mod-- %! Switch to the previous workspace
-  , ((modm, xK_equal), nextWS  ) -- mod-= %! Switch to the next workspace
-  , ((modm, xK_j ), windows W.focusDown >> warpToWindow' ) -- %! Move focus to the next window
-  , ((modm, xK_k ), windows W.focusUp >> warpToWindow' ) -- %! Move focus to the previous window
-  , ((modm, xK_m ), windows W.focusMaster >> warpToWindow' ) -- %! Move focus to the master window
-  , ((modm .|. shiftMask, xK_f ), sendMessage $ Toggle MIRROR) -- toggle mirroring of active layout
-  , ((modm, xK_f ), sendMessage $ Toggle FULL) -- toggle mirroring of active layout
+  , ((modm, xK_a      ), selectWorkspace def)
+  , ((modm, xK_s      ), withWorkspace def (windows . W.shift))
+  , ((modm, xK_d      ), renameWorkspace def)
+  , ((modm, xK_f), prevWS  ) -- mod-- %! Switch to the previous workspace
+  , ((modm, xK_g), nextWS  ) -- mod-= %! Switch to the next workspace
+
+  -- , ((modm, xK_j ), windows W.focusDown >> warpToWindow' ) -- %! Move focus to the next window
+  -- , ((modm, xK_k ), windows W.focusUp >> warpToWindow' ) -- %! Move focus to the previous window
+  -- , ((modm, xK_m ), windows W.focusMaster >> warpToWindow' ) -- %! Move focus to the master window
+  -- bindings for SubLayouts navigation
+  -- moving windows
+  -- , ((modm .|. controlMask, xK_h), sendMessage $ pullGroup L)
+  -- , ((modm .|. controlMask, xK_l), sendMessage $ pullGroup R)
+  -- , ((modm .|. controlMask, xK_k), sendMessage $ pullGroup U)
+  -- , ((modm .|. controlMask, xK_j), sendMessage $ pullGroup D)
+  -- merging windows
+  -- , ((modm .|. controlMask, xK_m), withFocused (sendMessage . MergeAll))
+  -- , ((modm .|. controlMask, xK_u), withFocused (sendMessage . UnMerge))
+  -- adjusting focus
   ]
   ++
-  zip (zip (repeat (modm)) [xK_1..xK_9]) (map (withNthWorkspace W.greedyView) [0..])
-  ++
-  zip (zip (repeat (modm .|. shiftMask)) [xK_1..xK_9]) (map (withNthWorkspace W.shift) [0..])
-  ++
+  -- zip (zip (repeat (modm)) [xK_1..xK_9]) (map (withNthWorkspace W.greedyView) [0..])
+  -- ++
+  -- zip (zip (repeat (modm .|. shiftMask)) [xK_1..xK_9]) (map (withNthWorkspace W.shift) [0..])
+  -- ++
   -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
   -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
   --[((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
   [((m .|. modm, key), screenSwitchAndWarp sc f)
-      | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+      | (key, sc) <- zip [xK_q, xK_w, xK_e] [0..]
       , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 warpToWindow' = warpToWindow (1/2) (1/2)
@@ -86,15 +128,22 @@ myLogHook xmprocList = dynamicLogWithPP xmobarPP
   , ppTitle  = xmobarColor "green" "" . shorten 50
 }
 
-myLayout = mkToggle (FULL ?? EOT) . mkToggle (single MIRROR) $ (tiled ||| myTabbed)
-  where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = renamed [Replace "Tiled"] $ Tall nmaster delta ratio
-     -- The default number of windows in the master pane
-     nmaster = 1
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
-     -- tabbed layout
-     myTabbed = renamed [Replace "NiceTabbed"] $ layoutN 1 (relBox 0 0 0.5 1) (Just $ relBox 0 0 1 1) (Tall 0 0.01 0.5) (layoutAll (relBox 0.5 0 1 1) (tabbed shrinkText def))
+-- myLayout = addTabs shrinkText def
+--          $ subLayout [0,1,2] (Full ||| Tall 1 0.2 0.5 ||| Full)
+--          $ Tall 1 0.2 0.5 ||| Full
+
+myLayout = mkToggle (FULL ?? EOT) . mkToggle (single MIRROR) $ windowNavigation $ subTabbed $ boringWindows $
+                       Tall 1 0.03 0.5
+
+-- myLayout = mkToggle (FULL ?? EOT) . mkToggle (single MIRROR) $ (tiled ||| myTabbed)
+--   where
+--      -- default tiling algorithm partitions the screen into two panes
+--      tiled   = renamed [Replace "Tiled"] $ Tall nmaster delta ratio
+--      -- The default number of windows in the master pane
+--      nmaster = 1
+--      -- Default proportion of screen occupied by master pane
+--      ratio   = 1/2
+--      -- Percent of screen to increment by when resizing panes
+--      delta   = 3/100
+--      -- tabbed layout
+--      myTabbed = renamed [Replace "NiceTabbed"] $ layoutN 1 (relBox 0 0 0.5 1) (Just $ relBox 0 0 1 1) (Tall 0 0.01 0.5) (layoutAll (relBox 0.5 0 1 1) (tabbed shrinkText def))
